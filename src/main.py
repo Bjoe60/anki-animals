94dbf038909a140249bdcbddf9d1a69094271e8e
Bjørn <s204451@student.dtu.dk>
2024-11-15 23:22:08 +0100

    Adding images

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from get_species import get_species
 from get_translations import get_translations
+from get_images import get_images
 
 def main():
     # get_species()
-    get_translations()
+    # get_translations()
+    get_images()
 
 
 if __name__ == '__main__':
