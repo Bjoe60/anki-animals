58bedb51bba8c574835846c1b9fc0ca0311577dc
Bjørn <s204451@student.dtu.dk>
2024-11-23 09:32:19 +0100

    Add sort strings

--- src/combine_data.py
+++ src/combine_data.py
@@ -2,10 +2,13 @@ import pandas as pd
 import os
 from functools import reduce
 
+def create_sort_string(df, name):
+    df[name] = df.reset_index(drop=True).index.map(lambda i: f"{i:06d}")
+
 def create_csv(df):
     with open(os.path.join('data', 'animals.csv'), 'w', encoding='utf-8', newline='') as f:
         # File header for Anki
-        f.write('#separator:Comma\n#html:true\n#notetype:Species\n#deck:Animals\n#tags column:3\n#columns:EOL ID,Scientific,Tags,iNaturalist ID,GBIF ID,English,Afrikaans,Albanian,Arabic,Armenian,Azerbaijani,Belarusian,Bengali,Bulgarian,Catalan,Chinese,Croatian,Czech,Danish,Dutch,Estonian,Finnish,French,Galician,Georgian,German,Greek,Hebrew,Hungarian,Icelandic,Indonesian,Italian,Japanese,Kazakh,Korean,Latvian,Lithuanian,Macedonian,Malay,Maltese,Mongolian,Nepali,Norwegian,Persian,Polish,Portuguese,Romanian,Russian,Serbian,Slovak,Slovenian,Spanish,Swahili,Swedish,Thai,Turkish,Ukrainian,Uzbek,Vietnamese,Countries,Images,extinct,Observations,Wikipedia,Summary\n')
+        f.write('#separator:Comma\n#html:true\n#notetype:Species\n#deck:Animals\n#tags column:3\n#columns:EOL ID,Scientific,Tags,iNaturalist ID,GBIF ID,English,Afrikaans,Albanian,Arabic,Armenian,Azerbaijani,Belarusian,Bengali,Bulgarian,Catalan,Chinese,Croatian,Czech,Danish,Dutch,Estonian,Finnish,French,Galician,Georgian,German,Greek,Hebrew,Hungarian,Icelandic,Indonesian,Italian,Japanese,Kazakh,Korean,Latvian,Lithuanian,Macedonian,Malay,Maltese,Mongolian,Nepali,Norwegian,Persian,Polish,Portuguese,Romanian,Russian,Serbian,Slovak,Slovenian,Spanish,Swahili,Swedish,Thai,Turkish,Ukrainian,Uzbek,Vietnamese,Countries,Images,extinct,Observations,Wikipedia,Summary,Taxonomic sort,Observations sort\n')
         
         df.to_csv(f, index=False)
 
@@ -25,6 +28,14 @@ def combine_data():
     # Create tags
     df_merged['higherClassification'] = df_merged['higherClassification'].str.replace('Life|Cellular Organisms|Eukaryota|Opisthokonta|Metazoa|Bilateria|Deuterostomia|Chordata|Vertebrata|Gnathostomata|Osteichthyes|Sarcopterygii|Tetrapoda|Amniota|Synapsida|Therapsida|Cynodontia|', '').str.replace('|', '::').str.replace(' ', '-')
     df_merged['higherClassification'] = df_merged['country'] + ' ' + df_merged['higherClassification']
+
+    # Create strings to sort by in Anki
+    create_sort_string(df_merged, 'taxonomic_sort')
+    
+    df_merged.sort_values(by=['observations_count', 'taxonomic_sort'], ascending=[False, True], inplace=True)
+    create_sort_string(df_merged, 'observations_sort')
+    
+    df_merged.sort_values(by='taxonomic_sort', inplace=True)
     
     create_csv(df_merged)
     print("Done.")
